308a8a46c6791774d729c2ea290a9181aca5c815
Douglas Grace <dgracecenteno@gmail.com>
2025-12-01 14:07:34 -0300

    Creating a contact model and applying it to the database

--- ContactManagement/Data/ApplicationDbContext.cs
+++ ContactManagement/Data/ApplicationDbContext.cs
@@ -1,6 +1,31 @@
-﻿namespace ContactManagement.Data;
+﻿using ContactManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext
+namespace ContactManagement.Data;
+
+public class ApplicationDbContext : DbContext
 {
+    public DbSet<Contact> Contacts => Set<Contact>();
     
+    public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) : base(options) { }
+    
+    public  ApplicationDbContext() { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contact>()
+            .HasIndex(c => c.ContactNumber)
+            .IsUnique();
+
+        modelBuilder.Entity<Contact>()
+            .HasIndex(c => c.Email)
+            .IsUnique();
+
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Contact>().HasQueryFilter(c => !c.IsActive);
+        modelBuilder.Entity<Contact>().HasData(
+            new Contact { Id = 1, Name = "Exemplo Contacto", ContactNumber = "912345678", Email = "exemplo@teste.pt" }
+        );
+        
+    }
 }
--- ContactManagement/Models/Contact.cs
+++ ContactManagement/Models/Contact.cs
@@ -1,6 +1,20 @@
-﻿namespace ContactManagement.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagement.Models;
 
 public class Contact
 {
+    public int Id {get; set;}
     
+    [Required]
+    [MinLength(6, ErrorMessage = "The name must contain at least 6 letters.")]
+    public string Name { get; set; } = string.Empty;
+    
+    [Required]
+    [RegularExpression(@"^\d(9)$", ErrorMessage = "The contact number must have exactly 9 digits.")]
+    public string ContactNumber { get; set; } =  string.Empty;
+    
+    [EmailAddress] public string Email { get; set; } =  string.Empty;
+    
+    public bool IsActive {get; set;} = true;
 }
